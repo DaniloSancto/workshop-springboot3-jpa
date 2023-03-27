d8c08a19660fa4ad6a3dee43d8806faff6eda838
Danilo Santos <danilo.santos0404@hotmail.com>
2023-03-27 11:16:19 -0300

    Exception handling - delete

---
+++ src/main/java/com/danilosantos/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.danilosantos.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+
+}
